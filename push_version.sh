e419d09a41f74a2472ef8a2d005fbb1e15fe435d
RS-Credentive <robert.sherwood@credentive.com>
2023-12-28 13:36:08 -0500

    finalized push_version script

--- push_version.sh
+++ push_version.sh
@@ -1,4 +1,15 @@
-for file in tokenized_markdown/*.md; do
-    policyname=`echo $file | sed "s/tokenized_markdown\/\([[:alpha:]]*\)v[[:digit:].]*.md/\1/"`
-    echo cp $file tracked_policies/$policyname.md
+git checkout main
+
+for file in tokenized_markdown/*.md
+do
+    policyfile=`echo $file | sed "s/tokenized_markdown\/\([[:alpha:]]*\)v[[:digit:].]*.md/tracked_policies\/\1.md/"`
+    if  [[ ! -f "$policyfile" ]]
+    then
+        touch $policyfile
+        git add $policyfile
+    fi
+    git checkout -b $file
+    cp $file $policyfile
+    git add $policyfile
+    git commit -m $policyfile
 done
